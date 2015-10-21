44dc7aa036d05d7b835fbf26df94660ede750f9b
PvanHengel <PvanHengel@gmail.com>
2015-10-21 19:16:01 -0400

    Update rjava_config.sh

--- bin/rjava_config.sh
+++ bin/rjava_config.sh
@@ -7,7 +7,8 @@ export R_HOME=$R_BASE/lib64/R
 export JAVA_HOME=/app/vendor/openjdk-1.7.0_51
 export JAVA_CPPFLAGS="-I$JAVA_HOME/include -I$JAVA_HOME/include/linux"
 export R_INCLUDE=$R_HOME/include
-export PATH=$JAVA_HOME/bin:$R_BASE/bin:/app/vendor/gcc-$GCC_VERSION/bin:$PATH
+#export PATH=$JAVA_HOME/bin:$R_BASE/bin:/app/vendor/gcc-$GCC_VERSION/bin:$PATH
+export PATH=$JAVA_HOME/bin:$R_BASE/bin:/app/vendor/gcc/bin:$PATH
 
 export JAVA_HOME=/app/vendor/openjdk-1.7.0_51
 R CMD javareconf
